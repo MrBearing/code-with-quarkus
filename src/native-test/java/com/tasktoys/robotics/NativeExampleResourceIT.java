53017636f1b1011f0b7c5e74873c393d92cca725
quarkusio <no-reply@quarkus.io>
2020-05-06 16:08:28 +0000

    Initial commit

---
+++ src/native-test/java/com/tasktoys/robotics/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package com.tasktoys.robotics;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
