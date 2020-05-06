fe3d65db7620a3298bff9de6d91c22a6733e46c9
Takumi Okamoto <takumi1988okamoto@gmail.com>
2020-05-06 16:28:51 +0000

    add Auto injection

--- src/main/java/com/tasktoys/robotics/ExampleResource.java
+++ src/main/java/com/tasktoys/robotics/ExampleResource.java
@@ -1,12 +1,24 @@
 package com.tasktoys.robotics;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
 @Path("/hello")
 public class ExampleResource {
+
+    @Inject
+    GreetingService service;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/greeting/{name}")
+    public String greetings(@PathParam String name){
+        return service.greeting("HI! ,"+name);
+    }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
